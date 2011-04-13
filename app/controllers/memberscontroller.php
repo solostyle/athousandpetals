0dd9aa6db78c51135d66f14723a4970c97aa6ae6
solostyle <1style@gmail.com>
2011-04-14 00:30:06 +0530

    Added members controller for logging out

---
+++ app/controllers/memberscontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+class MembersController extends Controller {
+
+    function log_out() {
+
+    }
+    function login_woe() {
+        
+    }
+    function change_password() {
+        
+    }
+}
