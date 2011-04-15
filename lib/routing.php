2228509493a958cda12f58a69be11903487497e5
solostyle <1style@gmail.com>
2011-04-15 14:36:50 +0530

    Top-level links are routed to categories but the URLs don't have the word categories

--- lib/routing.php
+++ lib/routing.php
@@ -5,6 +5,9 @@ $routing = array(
                 '/^([0-9]{4}\/.*)/' => 'ids/index/\1',
                 '/^tags\/(.*?)/' => 'tags/index/\1',
                 '/^categories\/(.*?)/' => 'categories/index/\1',
+                '/^Massage_Therapy\/(.*?)/' => 'categories/index/Massage_Therapy/\1',
+                '/^Therapeutic_Yoga\/(.*?)/' => 'categories/index/Therapeutic_Yoga/\1',
+                '/^Meditation_Classes\/(.*?)/' => 'categories/index/Meditation_Classes/\1',
                 '/^about/' => 'passives/about',
                 '/^publishfeeds/' => 'passives/publishfeeds',
                  );
