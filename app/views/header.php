00f01b46a0f0a4ab33e69a7406676af3cadb14b7
solostyle <1style@gmail.com>
2011-05-10 19:22:11 +0530

    Creation of meditation, yoga, and massage controllers each with an empty index.

--- app/views/header.php
+++ app/views/header.php
@@ -70,9 +70,12 @@ if(isset($_POST['login_submit'])) {
 <?php echo $html->includeJs('yui28event');?>
 <?php echo $html->includeJs('yui28connection');?>
 <?php echo $html->includeJs('yui28dom');?>
-<?php echo $html->includeJs('iam');?>
-<?php echo $html->includeJs('iam.shell');?>
-<?php echo $html->includeJs('iam.admin');?>
+<?php echo $html->includeJs('atp');?>
+<?php echo $html->includeJs('atp.shell');?>
+<?php echo $html->includeJs('atp.admin');?>
+<?php echo $html->includeJs('atp.massage');?>
+<?php echo $html->includeJs('atp.yoga');?>
+<?php echo $html->includeJs('atp.meditation');?>
 <?php echo $html->includeCss('layout');?>
 <?php echo $html->includeCss('format');?>
 </head>
