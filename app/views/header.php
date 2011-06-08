b20893872e7f6c3de72870f7ab9cd002d3365503
solostyle <1style@gmail.com>
2011-06-08 20:17:36 +0530

    layout and format changes. WIDER

--- app/views/header.php
+++ app/views/header.php
@@ -86,11 +86,24 @@ if(isset($_POST['login_submit'])) {
     <!-- some lame tagline -->
     <p id="pagesubtitle"><em>Massage Therapist, Yoga and Meditation Teacher</em></p>
 
-    <!-- show some main navigation -->
-    <ul id="mainnav"><?php 
+    <!-- show top-level navigation -->
+    <ul id="topnav"><?php 
         select_db();
         $cats = rtrv_categories();
-        echo make_list_item(make_link('About', make_url('about')));
+        //echo make_list_item(make_link('About', make_url('about'))); // removed for now june 8, 2011
+        foreach ($cats as $c) {
+            $link = str_replace(" ", "_", $c);
+            echo make_list_item(make_link($c, make_url($link.'/')));
+        }
+        mysql_close();
+            ?>
+    </ul>
+
+    <!-- show mid-level navigation -->
+    <ul id="midnav"><?php 
+        select_db();
+        $cats = rtrv_categories();
+        //echo make_list_item(make_link('About', make_url('about'))); // removed for now june 8, 2011
         foreach ($cats as $c) {
             $link = str_replace(" ", "_", $c);
             echo make_list_item(make_link($c, make_url($link.'/')));
