c979f1ba6e7fee3be553184cdfb25d895c46707a
solostyle <1style@gmail.com>
2011-06-10 16:22:23 +0530

    Some styling. Removed unused code from header.php and layout.css. Increased font size.

--- app/views/header.php
+++ app/views/header.php
@@ -81,7 +81,7 @@ if(isset($_POST['login_submit'])) {
 </head>
 <body>
 <div id="page">
-    <h2 id="pagetitle"><a href="/">Archana Sriram</a></h1>
+    <h1 id="pagetitle"><a href="/">Archana Sriram</a></h1>
 
     <!-- some lame tagline -->
     <p id="pagesubtitle"><em>Massage Therapist, Yoga and Meditation Teacher</em></p>
@@ -174,56 +174,3 @@ if(isset($_POST['login_submit'])) {
         <?php endif; ?>
 
     </div><!-- end div#login -->
-
-    <div id="content"> 
-        <div id="left">
-
-        </div><!-- end #left -->
-
-
-    <div id="right">
-        <?php if (isset($_SESSION['logged_in'])): ?>
-        <div id="blogAddForm">
-
-            <h2 id="addAnEntry">Add an Entry</h2>
-            <div id="addForm">
-                <div class="main">
-                    <p><input type="text" value="title" id="addFormTitle"/></p>
-                    <p><textarea id="addFormEntry">entry</textarea></p>
-                </div><!-- end .main -->
-
-                <div class="info">
-
-                    <!-- the category of the entry, radio buttons -->   
-                    <ul>
-                        <?php 
-                        select_db();
-                        $cats = rtrv_categories();
-                        mysql_close();
-                        foreach ($cats as $cat):?>
-                            <li><input type="radio" name="category" id="addFormCategory_<?php echo $cat?>" value="<?php echo $cat?>" /><?php echo $cat?></li>
-                        <?php endforeach;?>
-                    </ul>
-
-
-                    <!-- the date and time, empty, filled in with javascript -->
-                    <p>
-                        <input type="text" id="year" name="year" size="3" maxlength="4" value="" />
-                        <input type="text" id="month" name="month" size="1" maxlength="2" value="" />
-                        <input type="text" id="date" name="date" size="1" maxlength="2" value="" />
-                        <input type="text" id="hour" name="hour" size="1" maxlength="2" value="" />
-                        <input type="text" id="minute" name="minute" size="1" maxlength="2" value="" />
-                    </p>
-                    <p>
-                        <input type="button" id="addFormChangeTime" value="Change" />
-                    </p>
-                    <p>
-                        <input type="text" readonly="readonly" id="addFormTime" size="20" value="" />
-                    </p>
-
-                    <p><input type="button" id="addFormSubmit" value="add"/></p>
-                </div><!-- end .info -->
-            </div><!-- end #addForm -->
-
-        </div><!-- end $blogAddForm -->
-        <?php endif; ?>
--- app/views/massagepages/therapies.php
+++ app/views/massagepages/therapies.php
@@ -1,7 +1,26 @@
-    <div id="contentWP">
+<div id="content"> 
+   <div id="left">
+   <ul>
+   <?php 
+   $lists = array('Vivamus imperdiet', 'Aenean in lorem vel purus porttitor.', 'Phasellus at neque id sapien dapibus ornare.', 'Nulla pharetra orci laoreet turpis faucibus sagittis ut et metus.');
 
-    </div><!-- end #contentWP -->
-</div><!-- end #right -->
+foreach ($lists as $l) {
+  $link = str_replace(" ", "_", $l);
+  echo make_list_item(make_link($l, make_url($link.'/')));
+}
+?>
+</ul>
+    </div><!-- end #left -->
+
+    <div id="right">
+<h1>Lorem Ipsum</h1>
+
+<p>Nunc tempus nunc quis lorem pretium ut porttitor nunc consectetur. Quisque pellentesque condimentum massa. Nulla quis enim vitae est egestas porta ac ac tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat mi imperdiet eros fringilla placerat eleifend felis molestie. Ut consectetur interdum pharetra. Nunc mattis fringilla leo, quis fermentum nulla rhoncus vitae. Donec rhoncus urna sit amet eros cursus condimentum. Nam non tortor nisl. Nam eu mauris arcu. Mauris sit amet tempus mi. Aenean vitae odio massa, at rhoncus orci. Phasellus euismod consectetur tellus malesuada imperdiet. </p>
+
+<h2>Dolor sit Amet</h2>
+<p>Etiam gravida massa eu risus tincidunt nec posuere orci pulvinar. Nunc diam sem, dignissim quis fermentum a, tempus sit amet nisi. Vestibulum porttitor magna quis odio dignissim pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc blandit, nulla id volutpat interdum, mauris nulla pellentesque leo, quis viverra tellus dui a nulla. Vestibulum id arcu vitae mauris fermentum pulvinar id nec augue. Morbi non placerat augue. Pellentesque vulputate porttitor nibh, at imperdiet massa luctus quis. Suspendisse scelerisque turpis lacus. Mauris ut dui nisi. Integer mollis metus et enim fringilla hendrerit. Nulla nec erat non ipsum interdum aliquet ut eu erat. Donec semper, lacus eu facilisis mattis, libero purus lacinia dolor, ac pulvinar leo eros ut libero. </p>
+
+    </div><!-- end #right -->
 
 <script type="text/javascript">
 
