1905ce83bb542a2be560b049d8f034dc2ea2b6c3
solostyle <1style@gmail.com>
2011-06-10 23:22:23 +0530

    Added CONTENT to massage therapy

--- app/views/massagepages/therapies.php
+++ app/views/massagepages/therapies.php
@@ -2,23 +2,38 @@
    <div id="left">
    <ul>
    <?php 
-   $lists = array('Vivamus imperdiet', 'Aenean in lorem vel purus porttitor.', 'Phasellus at neque id sapien dapibus ornare.', 'Nulla pharetra orci laoreet turpis faucibus sagittis ut et metus.');
+   $lists = array('Relaxation and Circulatory', 'Clinical Rehabilitative', 'Sports and Event', 'Myofascial Structural', 'Deep Tissue');
 
 foreach ($lists as $l) {
   $link = str_replace(" ", "_", $l);
-  echo make_list_item(make_link($l, make_url($link.'/')));
+  echo make_list_item(make_link($l, make_url('Massage_Therapy/therapies/'.$link)));
 }
 ?>
 </ul>
     </div><!-- end #left -->
 
     <div id="right">
-<h1>Lorem Ipsum</h1>
 
-<p>Nunc tempus nunc quis lorem pretium ut porttitor nunc consectetur. Quisque pellentesque condimentum massa. Nulla quis enim vitae est egestas porta ac ac tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat mi imperdiet eros fringilla placerat eleifend felis molestie. Ut consectetur interdum pharetra. Nunc mattis fringilla leo, quis fermentum nulla rhoncus vitae. Donec rhoncus urna sit amet eros cursus condimentum. Nam non tortor nisl. Nam eu mauris arcu. Mauris sit amet tempus mi. Aenean vitae odio massa, at rhoncus orci. Phasellus euismod consectetur tellus malesuada imperdiet. </p>
+  <h1>Swedish Relaxation and Circulatory Massage</h1>
+  <p>The perfect stress reliever, the Swedish Relaxation and Circulatory Massage nourishes those tired, stiff areas, bringing every part of your body back to harmony and balance. This massage stimulates the relaxation centers of the nervous system to help remove stress-related toxins, leaving you with a calmer, freer mind.</p>
+  <p>With presence and attention to every body part, your brain begins to reconnect with your body as your body unwinds to its natural state. The balanced mind-body is how you were meant to be. As you rediscover this holistic balance between mind and body, you will engage the world with greater body awareness, and you will face your life's challenges with a fresh, optimistic perspective.</p>
+<p>The wonderful respite from your daily stresses that this massage brings is a welcome addition to your regular self-care. Whether you decide to have one weekly, bi-weekly, or monthly, you will see positive changes in your life with regular Swedish Relaxation and Circulatory Massage!</p>
 
-<h2>Dolor sit Amet</h2>
-<p>Etiam gravida massa eu risus tincidunt nec posuere orci pulvinar. Nunc diam sem, dignissim quis fermentum a, tempus sit amet nisi. Vestibulum porttitor magna quis odio dignissim pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc blandit, nulla id volutpat interdum, mauris nulla pellentesque leo, quis viverra tellus dui a nulla. Vestibulum id arcu vitae mauris fermentum pulvinar id nec augue. Morbi non placerat augue. Pellentesque vulputate porttitor nibh, at imperdiet massa luctus quis. Suspendisse scelerisque turpis lacus. Mauris ut dui nisi. Integer mollis metus et enim fringilla hendrerit. Nulla nec erat non ipsum interdum aliquet ut eu erat. Donec semper, lacus eu facilisis mattis, libero purus lacinia dolor, ac pulvinar leo eros ut libero. </p>
+<h1>Clinical Rehabilitative Massage</h1>
+<p>Soft tissue injuries are more common than we realize. Sometimes we aren't even aware of painful areas until they are touched. Clinical Rehabilitative Massage provides the in-depth attention that you need to address the injury and all the affected tissue systems. You can expect a thorough intake of your situation, including posture and functional analysis, a variety of massage techniques from different modalities, and suggestions for your self-care outside of the session room--all informed by a clear understanding of anatomy and tissue depth, and tailored to your particular condition. Your discomfort will melt away as your body repairs itself in the most optimal way with Clinical Rehabilitative Massage.</p>
+  <p>If you receive regular massage, you probably know that regular circulatory massage can give you the awareness of an injured area before the injury escalates or complicates, and that it can even remedy minor injuries. But sometimes we are afflicted by age-old injuries that need special attention, or sudden accidents that we would like to heal quickly. A more clinical approach may be necessary to attend to traumatized tissue.</p>
+  <p>Some conditions that indicate massage:</p>
+  <ul><li>Muscle Strains</li><li>Ligament Sprains</li><li>Bursitis</li><li>Tendonitis</li><li>Tendinosis</li><li>Trigger Points</li><li>Tender Points</li><li>Tennis Elbow (medial epicondylitis)</li><li>Golfer Elbow (lateral epicondylitis)</li><li>Reduced range of motion</li><li></li></ul>
+  <p>If you are already seeking treatment for an injury, we can work with your health care provider to integrate massage therapy as part of your plan. Book a free consultation to talk about these concerns, so we can create the best plan for your care.</p>
+
+  <h1>Sports and Event Massage</h1>
+  <p></p>
+
+  <h1>Myofascial Structural</h1>
+  <p></p>
+
+  <h1>Deep Tissue</h1>
+  <p></p>
 
     </div><!-- end #right -->
 
