c097a13e5c920426dd29ea1496120d6a1d8249b4
solostyle <spam@solostyle.net>
2011-07-26 19:14:53 -0500

    More content for massage pages added

--- app/views/massagepages/benefits.php
+++ app/views/massagepages/benefits.php
@@ -1,8 +1,32 @@
-    <div id="contentWP">
+<div id="content"> 
+  <div id="left">
 
-    </div><!-- end #contentWP -->
-</div><!-- end #right -->
+  </div><!-- end #left -->
 
-<script type="text/javascript">
+  <div id="right">
+  
+   <h1>Benefits of ATP Massage Therapy</h1>
+    <p>The benefits of massage range from the physical to the psychological and spiritual. Different kinds of massage work with different structures with different intention. The benefits of all massage, including circulatory and relaxation massage, include</p>
+	<ul>
+		<li>mental and physical relaxation</li>
+		<li>pain relief</li>
+		<li>enhanced circulation</li>
+		<li>reduced muscle tension</li>
+		<li>enchanced body awareness</li>
+		<li>improved overall well-being</li>
+	</ul>
+	<p>Specific and targeted work with therapeutic intention has these additional benefits:</p>
+	<ul>
+		<li>muscle lengthening</li>
+		<li>structural balance</li>
+		<li>longer-lasting change</li>
+		<li>fluid balance of tissues</li>
+		<li>significant pain reduction</li>
+		<li>increased speed of tissue healing</li>
+	</ul>
+	<p>Our massage therapies range from circulatory and relaxation to more specific and targeted work, often combining approaches for your unique needs. </p>
+  </div><!-- end #right -->
 
-</script>
+  <script type="text/javascript">
+
+  </script>
