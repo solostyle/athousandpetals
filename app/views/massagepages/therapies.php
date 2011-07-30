e0e1de541c9900d7b5510832297137962c826af1
solostyle <spam@solostyle.net>
2011-07-29 21:46:15 -0500

    Left links still not working

--- app/views/massagepages/therapies.php
+++ app/views/massagepages/therapies.php
@@ -2,11 +2,12 @@
   <div id="left">
     <ul>
       <?php 
-	 $lists = array('Relaxation and Circulatory', 'Clinical Rehabilitative', 'Sports and Event', 'Myofascial Structural', 'Deep Tissue');
+	 $lists = array('Swedish Relaxation and Circulatory', 'Clinical Rehabilitative', 'Sports and Event', 'Myofascial Structural', 'Deep Tissue');
 
 	 foreach ($lists as $l) {
-	 $link = str_replace(" ", "_", $l);
-	 echo make_list_item(make_link($l, make_url('Massage_Therapy/therapies/'.$link)));
+		$words = explode(" ",$l,2);
+		$link = strtolower($words[0]);
+		echo make_list_item(make_link($l, make_url('Massage_Therapy/therapies/#'.$link)));
 	 }
 	 ?>
     </ul>
@@ -14,26 +15,26 @@
 
   <div id="right">
 
-    <h1>Swedish Relaxation and Circulatory Massage</h1>
+    <h1 name="swedish">Swedish Relaxation and Circulatory Massage</h1>
     <p>The perfect stress reliever, the Swedish Relaxation and Circulatory Massage nourishes those tired, stiff areas, bringing every part of your body back to harmony and balance. This massage stimulates the relaxation centers of the nervous system to help remove stress-related toxins, leaving you with a calmer, freer mind.</p>
     <p>With presence and attention to every body part, your brain begins to reconnect with your body as your body unwinds to its natural state. The balanced mind-body is how you were meant to be. As you rediscover this holistic balance between mind and body, you will engage the world with greater body awareness, and you will face your life's challenges with a fresh, optimistic perspective.</p>
     <p>The wonderful respite from your daily stresses that this massage brings is a welcome addition to your regular self-care. Whether you decide to have one weekly, bi-weekly, or monthly, you will see positive changes in your life with regular Swedish Relaxation and Circulatory Massage!</p>
 
-    <h1>Clinical Rehabilitative Massage</h1>
+    <h1 name="clinical">Clinical Rehabilitative Massage</h1>
     <p>Soft tissue injuries are more common than we realize. Sometimes we aren't even aware of painful areas until they are touched. Whether or not you have a known soft tissue condition, we can help you restore the fluidity and balance. Clinical Rehabilitative Massage provides the in-depth attention that you need to address the injury and all the affected tissue systems. You can expect a thorough intake of your situation, including posture and functional analysis, a variety of massage techniques from different modalities, and suggestions for your self-care outside of the session room--all informed by a clear understanding of anatomy and tissue depth, and tailored to your particular condition. Your discomfort will melt away as your body repairs itself in the most optimal way with Clinical Rehabilitative Massage.</p>
     <p>If you receive regular massage, you probably know that regular circulatory massage can give you the awareness of an injured area before the injury escalates or complicates, and that it can even remedy minor injuries. But sometimes we are afflicted by age-old injuries that need special attention, or sudden accidents that we would like to heal quickly. A more clinical approach may be necessary to attend to traumatized tissue.</p>
     <p>Some conditions that indicate massage:</p>
     <ul><li>Muscle Strains</li><li>Ligament Sprains</li><li>Bursitis</li><li>Tendonitis</li><li>Tendinosis</li><li>Trigger Points</li><li>Tender Points</li><li>Tennis Elbow (medial epicondylitis)</li><li>Golfer Elbow (lateral epicondylitis)</li><li>Reduced range of motion</li><li>Postural deviations</li></ul>
     <p>If you are already seeking treatment for an injury, we can work with your health care provider to integrate massage therapy as part of your plan. Book a free consultation to talk about these concerns, so we can create the best plan for your care.</p>
 
-    <h1>Sports and Event Massage</h1>
+    <h1 name="sports">Sports and Event Massage</h1>
     <p>When it comes to athletes, massage can be a great performance enhancer. You have a routine of training and working out, and you need massage work that fits into that routine and works with your sport. Massage has so much to offer athletes. It flushes the by-products of energy metabolism from your tissues, restoring your body's fluid balance. It speeds muscle repair after strenuous training. It calms the nervous system so that repair and healing can take place efficiently. It prevents overuse injuries by balancing your muscles and encouraging the proper alignment of your joints. Most overuse injuries can be prevented with a mixture of massage and self-care exercises to keep your body balanced. And, of course, with regular massage work comes greater body awareness which will give you insight into the changes that are happening in your body as you train. This increased awareness improves your ability to recruit the muscles you need in your sport and keeps you from over-extending or injuring yourself. A combination of circulatory, neuromuscular, and clinical massage techniques are used to give you the best custom-tailored Sports Massage you need.</p>
     <p>For your next race, game, or sports event, you can integrate massage as part of your warm-up and cool-down. Pre-event massage energizes your body, in particular the muscle groups that will be most used, and gets you ready for action. If your event requires you to be on the bench or rest in between, some inter-event massage is what you need to restore your energy and keep you going. Post-event massage helps with recovery, repair, and relaxation because it focuses on increasing circulation of nutrients through your tissues and prevents soreness and cramping. It is done within two hours of your event. Within the three days after an athletic event, restorative massage resets the length of muscles shortened by heavy exercise. These types of massage are not replacements for your regular warm-up and cool-down routines, but it can be integral to your performance.</p>
 	<p>Maintenance massage is for ongoing regular care during your athletic training. It addresses the myofascial and neuromuscular systems of the body with special attention to the areas you use most.</p>
     <p>Treatment massage is also available if you sustain any soft tissue injuries that prevent you from training. It is a targeted clinical approach to speed repair as you work toward resuming your normal training.</p>
     <p>Sports Massage is an excellent addition to your training and can help you discover your full potential!</p>
 
-    <h1>Myofascial, Structural Massage</h1>
+    <h1 name="myofascial">Myofascial, Structural Massage</h1>
     <h2>What is Myofascial Massage?</h2>
     <p>Pronounced "my'-oh-fa'-shel", myofascial massage works with the connective tissue structure of your musculoskeletal system. Fascia ("fa'-sha") is a type of connective tissue that layers under the skin and around muscles, bones, ligaments, and tendons. It also forms the structure of every cell of the body. Fascia forms trains of fibers that are aligned in different directions, creating an interconnected web of fibers. This web works with muscles and bones to keep the body upright using opposing forces of tension (see Tensegrity). At the microscopic level, fascia is made up of non-living fibers embedded in a matrix that can change from fluid to gel, gel to fluid. Other structures also pass through this structure, like nerve fibers and blood and lymph vessels. So when the consistency of fascia changes from fluid to gel, it can harden and interfere with the flow of circulation and nerve signals. Though fascia is extremely strong and supports the structures in the body, it is also meant to be elastic and flexible to allow for mobility.</p>
     <p>Myofascial massage works to free this fascia and restructure the body.</p>
@@ -47,7 +48,7 @@
     <p>Myofascial work is slow and requires patience. The goal is not to increase the blood flow with quick movements, but to melt the tissue and allow it to move on its own. The fascia is not forced but <em>encouraged</em> to move as the therapist applies firm, steady engagement. As your body does most of the work, you will want to use deep, slow, full, relaxed breathing and focus your awareness on the small changes taking place. Breathing and awareness on your part allow for greater energy flow so that change can occur. You can also participate by communicating how you feel, what you feel, and where you feel it, or you can remain silent and present. Some people notice tingling sensations in the part of the body being addressed or sensations in a completely different area. Some people feel increased heat in some areas, or an opening of the joints. If anything is uncomfortable, just let your therapist know.</p>
     <p>After the massage, you should drink fluids to keep the tissues hydrated. Especially with myofascial work, you may want to do posture alignment exercises to support the changes made in the session.</p>
 
-    <h1>Deep Tissue Massage</h1>
+    <h1 name="deep">Deep Tissue Massage</h1>
     <h2>What is Deep Tissue Massage?</h2>
     <p>The term "deep tissue" is a misleading term that most people think means a massage that involves digging into the muscles with an elbow. People who think this is a "real" massage have probably been frustrated by massages that only work the surface layers without reaching the deeper ones, especially in areas that need it. To receive superficial work over a spot that wants deeper work is more frustrating than not receiving work at all! Thus many people want something called "deep tissue" when they actually just want a good massage.</p>
     <p>Actually all of the massages listed here can be deep tissue massages. What is a deep tissue? It is merely a tissue that is located at a deeper layer than another tissue. A good massage therapist knows how to access the different layers of your body and knows what lies in each layer. Take for example the muscles on your scapula (shoulder blade). On most of the scapula, there is only one layer of muscle, the infraspinatus. Any deeper than this, and you would be hitting bone. However in many regions of the posterior hip, the layers are several inches deep and each layer's fibers run in different directions. Here's where "deep tissue" has meaning. Deep work is the only way to access deep tissue, but <em>good</em> work can access tissue at any level.</p>
