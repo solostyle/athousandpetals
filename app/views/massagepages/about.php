ea89889e8b573d1ca17f777af5af691fc3ee999c
solostyle <spam@solostyle.net>
2011-11-15 23:59:50 -0600

    Part of last commit

--- app/views/massagepages/about.php
+++ app/views/massagepages/about.php
@@ -5,7 +5,9 @@
   <div id="right">
     
    	<h3>Yoga and Meditation Instruction</h3>
-	<p>As a student and practitioner of the internal arts for many years, I have come to appreciate the value of consistent practice on our innate healing powers. The Chinese Qi Gong tradition was my first formal introduction to meditation, which I have integrated with the Yoga tradition. I also incorporate the principles of Patanjali's Ashtanga Yoga into my Hatha Yoga practice. I believe that my dedicated personal practice of these arts has improved all aspects of my life, and it contributes to the joy I have for teaching. As most of my understanding comes from direct experience, I guide my students to seek knowledge through their own experience. My scientific and experiential understanding of human anatomy informs my instruction, and my training as a massage therapist gives me a creative, therapeutic approach that focuses on your individual situation.</p>
+	<p>As a student and practitioner of the internal arts for many years, My first love of movement came from when I learned gymnastics as a child. Years later I found a book containing yoga postures that inspired me to start my own practice. I also became aware of yogic and meditative practices through my grandfather and my family's traditional Indian culture. For over a decade I sought an authentic approach to yoga and at last discovered a shala in Mysore, India that appealed to me. In 2011 I studied with Bharath Shetty for two months and continued the following three months on my own, deepening and personalizing my practice. I have come to appreciate the value of consistent practice on our innate healing powers.</p>
+	<p>The Chinese Qi Gong tradition was my first formal introduction to meditation, which I have integrated with the Yoga tradition. I also incorporate the principles of Patanjali's Ashtanga Yoga into my Hatha Yoga practice. I believe that my dedicated personal practice of these arts has improved all aspects of my life, and it contributes to the joy I have for teaching. As most of my understanding comes from direct experience, I guide my students to seek knowledge through their own experience. My scientific and experiential understanding of human anatomy informs my instruction, and my training as a massage therapist gives me a creative, therapeutic approach that focuses on your individual situation.</p>
+	<p>
 
 	<h3>Massage Therapy</h3>
 	<!-- <p>the mind-body connection stimulates growth and repair
@@ -14,7 +16,7 @@
     you have a unique life and a unique coping strategy with your life's challenges. whatever you have done until this moment has been what you had to do. if you are alive today you are a success. everything about your life is recorded in your body in some way. because of this, each person needs a different kind of massage
     massage therapy is supportive and healing
 -->
-    <p>ATP Massage Therapy offers Swedish Relaxation and Cirulatory Massage, Clinical Rehabilitative Massage, Sports and Event Massage, and Myofascial Structural Massage.</p>
+    <p>I graduated valedictorian from the Cortiva Institute in downtown Chicago in December of 2010. I offer Swedish Relaxation and Cirulatory Massage, Clinical Rehabilitative Massage, Sports and Event Massage, and Myofascial Structural Massage.</p>
 	
   </div><!-- end #right -->
 </div<<!-- end #content -->
